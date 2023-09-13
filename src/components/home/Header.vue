509fe2878758ec783be75341167b0689db73aa7d
Johnny-zbb <54133977+Johnny-zbb@users.noreply.github.com>
2023-09-13 16:13:07 +0800

    绘制工具开发
    完成基本样式

--- src/components/home/Header.vue
+++ src/components/home/Header.vue
@@ -3,8 +3,8 @@
         <div class="systemName">
             SuperGIS
         </div>
-        <div class="icon">
-            <n-icon size="18" color="white" :component="IosCalendar" />
+        <div class="icon" @click="handleClick">
+            <n-icon size="18" color="white" :component="MdAddCircle" />
         </div>
         <div class="icon">
             <n-icon size="18" color="white" :component="IosClock" />
@@ -16,25 +16,61 @@
             <n-icon size="18" color="white" :component="IosSettings" />
         </div>
         <div class="icon">
-            <span style="cursor: default;">{{dateString||'8月14日 周一 18:00'}}</span>
+            <span style="cursor: default;">{{ dateString || '8月14日 周一 18:00' }}</span>
         </div>
     </div>
+    <n-popover :show="showPopoverRef" :x="xRef" :y="yRef" trigger="manual" :show-arrow="false">
+        <template #header>
+            <n-text strong depth="1">
+                绘制工具
+            </n-text>
+        </template>
+        <div style="
+        /* width: 70px; */
+        display: flex;
+        justify-content: space-around;
+        background-color: white;
+        transform-origin: inherit;
+      ">
+            <n-button size="medium" secondary strong>绘制点<n-icon size="18" color="black" :component="IosPin" /></n-button>
+            <n-button size="medium" secondary strong>绘制线<n-icon size="18" color="black"
+                    :component="IosTrendingUp" /></n-button>
+            <n-button size="medium" secondary strong>绘制面<n-icon size="18" color="black"
+                    :component="MdCellular" /></n-button>
+        </div>
+    </n-popover>
 </template>
 
 <script setup>
-import { IosCalendar, IosClock, IosCloudy, IosSettings, IosSwitch } from '@vicons/ionicons4'
-import { NIcon } from 'naive-ui'
+import { MdAddCircle, IosClock, IosCloudy, IosSettings, IosSwitch, IosTrendingUp, IosPin, MdCellular } from '@vicons/ionicons4'
+import { NIcon, NPopover, NButton } from 'naive-ui'
 import { getToday } from '@/methods/webTools.ts'
-import { onMounted, ref ,onBeforeUnmount,} from 'vue'
+import { onMounted, ref, onBeforeUnmount, } from 'vue'
 let dateString = ref('')
 let timer = null
+
+const xRef = ref(0)
+const yRef = ref(0)
+const showPopoverRef = ref(false)
+
+const handleClick = (e) => {
+    console.log(e)
+    if (showPopoverRef.value) {
+        showPopoverRef.value = false
+    } else {
+        showPopoverRef.value = true
+        xRef.value = e.clientX
+        yRef.value = 30
+    }
+}
+
 onMounted(() => {
     timer = setInterval(() => {
         dateString.value = getToday()
         console.log(dateString.value)
     }, 1000);
 })
-onBeforeUnmount(()=>{
+onBeforeUnmount(() => {
     clearInterval(timer)
 })
 </script>
